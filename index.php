e5d9ab88e75ce8beab97f10d508311d8b38e9086
Pablo de Dios <4855797@gmail.com>
2025-09-03 18:41:13 -0600

    app ixtla v2

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Inicio</title>
 </head>
 <body>
     Hola ixtla v1, servicios web
