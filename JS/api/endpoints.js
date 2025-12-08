4f62c793bcf90bacc8d8456ba00941322c625e66
shannon1052 <shannonkass.2112@gmail.com>
2025-12-08 15:38:54 -0600

    test endpoints

--- JS/api/endpoints.js
+++ JS/api/endpoints.js
@@ -2,43 +2,43 @@
 export const API = {
   // === Requerimiento ===
   REQUERIMIENTO: {
-    CREATE: "https://ixtlahuacan-fvasgmddcxd3gbc3.mexicocentral-01.azurewebsites.net/db/WEB/ixtla01_in_requerimiento.php",
-    UPDATE: "https://ixtlahuacan-fvasgmddcxd3gbc3.mexicocentral-01.azurewebsites.net/db/WEB/ixtla01_upd_requerimiento.php",
-    GET:    "https://ixtlahuacan-fvasgmddcxd3gbc3.mexicocentral-01.azurewebsites.net/db/WEB/ixtla01_c_requerimiento.php",
+    CREATE: "ixtla-app.com/db/WEB/ixtla01_in_requerimiento.php",
+    UPDATE: "ixtla-app.com/db/WEB/ixtla01_upd_requerimiento.php",
+    GET:    "ixtla-app.com/db/WEB/ixtla01_c_requerimiento.php",
   },
 
   // === Comentarios ===
   COMENTARIOS: {
-    CREATE: "https://ixtlahuacan-fvasgmddcxd3gbc3.mexicocentral-01.azurewebsites.net/db/WEB/ixtla01_i_comentario_requerimiento.php",
-    UPDATE: "https://ixtlahuacan-fvasgmddcxd3gbc3.mexicocentral-01.azurewebsites.net/db/WEB/ixtla01_u_comentario_requerimiento.php",
-    LIST:   "https://ixtlahuacan-fvasgmddcxd3gbc3.mexicocentral-01.azurewebsites.net/db/WEB/ixtla01_c_comentario_requerimiento.php",
+    CREATE: "ixtla-app.com/db/WEB/ixtla01_i_comentario_requerimiento.php",
+    UPDATE: "ixtla-app.com/db/WEB/ixtla01_u_comentario_requerimiento.php",
+    LIST:   "ixtla-app.com/db/WEB/ixtla01_c_comentario_requerimiento.php",
   },
 
   // === Procesos ===
   PROCESOS: {
-    CREATE: "https://ixtlahuacan-fvasgmddcxd3gbc3.mexicocentral-01.azurewebsites.net/db/WEB/ixtla01_i_proceso_requerimiento.php",
-    UPDATE: "https://ixtlahuacan-fvasgmddcxd3gbc3.mexicocentral-01.azurewebsites.net/db/WEB/ixtla01_u_proceso_requerimiento.php",
-    LIST:   "https://ixtlahuacan-fvasgmddcxd3gbc3.mexicocentral-01.azurewebsites.net/db/WEB/ixtla01_c_proceso_requerimiento.php",
+    CREATE: "ixtla-app.com/db/WEB/ixtla01_i_proceso_requerimiento.php",
+    UPDATE: "ixtla-app.com/db/WEB/ixtla01_u_proceso_requerimiento.php",
+    LIST:   "ixtla-app.com/db/WEB/ixtla01_c_proceso_requerimiento.php",
   },
 
   // === Tareas ===
   TAREAS: {
     // DB/WEB/ixtla01_i_tarea_proceso.php
-    CREATE: "https://ixtlahuacan-fvasgmddcxd3gbc3.mexicocentral-01.azurewebsites.net/DB/WEB/ixtla01_i_tarea_proceso.php",
-    UPDATE: "https://ixtlahuacan-fvasgmddcxd3gbc3.mexicocentral-01.azurewebsites.net/db/WEB/ixtla01_u_tarea_proceso.php",
-    LIST:   "https://ixtlahuacan-fvasgmddcxd3gbc3.mexicocentral-01.azurewebsites.net/db/WEB/ixtla01_c_tarea_proceso.php",
+    CREATE: "ixtla-app.com/DB/WEB/ixtla01_i_tarea_proceso.php",
+    UPDATE: "ixtla-app.com/db/WEB/ixtla01_u_tarea_proceso.php",
+    LIST:   "ixtla-app.com/db/WEB/ixtla01_c_tarea_proceso.php",
   },
 
   // === Empleado ===
   EMPLEADOS: {
-    GET:    "https://ixtlahuacan-fvasgmddcxd3gbc3.mexicocentral-01.azurewebsites.net/db/WEB/ixtla01_c_empleado.php",
+    GET:    "ixtla-app.com/db/WEB/ixtla01_c_empleado.php",
   },
 
   // === Media ===
   MEDIA: {
     // DB\WEB\ixtla01_c_requerimiento_img.php
-    GET:    "https://ixtlahuacan-fvasgmddcxd3gbc3.mexicocentral-01.azurewebsites.net/DB/WEB/ixtla01_c_requerimiento_img.php",
+    GET:    "ixtla-app.com/DB/WEB/ixtla01_c_requerimiento_img.php",
     // DB\WEB\ixtla01_in_requerimiento_img.php
-    UPLOAD: "https://ixtlahuacan-fvasgmddcxd3gbc3.mexicocentral-01.azurewebsites.net/DB/WEB/ixtla01_in_requerimiento_img.php",
+    UPLOAD: "ixtla-app.com/DB/WEB/ixtla01_in_requerimiento_img.php",
   },
 };
